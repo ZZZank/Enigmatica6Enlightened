92ef6bf6b307981c2c111f03c2a2f95462031d3d
ZZZank <3410764033@qq.com>
2023-06-30 20:43:57 +0800

    rework items.js

--- kubejs/server_scripts/enigmatica/kubejs/expert/ae2_compat/shaped_items.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/ae2_compat/shaped_items.js
@@ -5,40 +5,33 @@ onEvent('recipes', (event) => {
     if (global.isExpertMode == false) {
         return;
     }
-    const id_prefix = 'enigmatica:expert/ae2_compat/items/';
-    const recipesIDs = [
-        'appliedenergistics2:network/cells/spatial_components',
-        'appliedenergistics2:tools/network_memory_card',
-        'appliedenergistics2:materials/cardspeed',
-        'appliedenergistics2:network/crafting/patterns_blank'
-    ];
-    recipesIDs.forEach((recipesID) => {
-        event.remove({ id: recipesID });
-    });
+    const id_prefix = 'enigmatica:expert/ae2/shaped_items/';
 
     const recipes = [
         {
-            output: '2_cubed_spatial_cell_component',
+            output: 'appliedenergistics2:2_cubed_spatial_cell_component',
             patterns: ['GFG', 'FAF', ' S '],
             inputs: {
                 G: 'minecraft:glowstone_dust',
                 A: 'refinedstorage:advanced_processor',
                 F: 'appliedenergistics2:fluix_pearl',
                 S: 'occultism:spirit_attuned_gem'
-            }
+            },
+            id: 'appliedenergistics2:network/cells/spatial_components'
         },
         {
-            output: 'memory_card',
+            output: 'appliedenergistics2:memory_card',
             patterns: ['   ', 'VMQ', 'GGG'],
             inputs: {
                 V: 'integrateddynamics:variable',
                 M: 'kubejs:memory_basic_filled',
                 Q: 'refinedstorage:quartz_enriched_iron',
                 G: 'minecraft:gold_ingot'
-            }
+            },
+            id: 'appliedenergistics2:tools/network_memory_card'
         },
         {
-            output: 'blank_pattern',
+            output: 'appliedenergistics2:blank_pattern',
             patterns: ['QGQ', 'GAG', 'RSR'],
             inputs: {
                 S: 'create:empty_schematic',
@@ -46,25 +39,26 @@ onEvent('recipes', (event) => {
                 G: 'minecraft:glowstone_dust',
                 R: 'refinedstorage:quartz_enriched_iron',
                 A: '#forge:gems/aquamarine'
-            }
+            },
+            id: 'appliedenergistics2:network/crafting/patterns_blank'
+        },
+        {
+            output: '4x appliedenergistics2:speed_card',
+            patterns: ['AGA', 'GLG', 'AGA'],
+            inputs: {
+                A: 'appliedenergistics2:advanced_card',
+                G: 'pneumaticcraft:glycerol',
+                L: {
+                    type: 'pneumaticcraft:fluid',
+                    tag: 'forge:lubricant',
+                    amount: 1000
+                }
+            },
+            id: 'appliedenergistics2:materials/cardspeed'
         }
     ];
 
     recipes.forEach((recipe) => {
-        event
-            .shaped('appliedenergistics2:' + recipe.output, recipe.patterns, recipe.inputs)
-            .id(id_prefix + recipe.output);
+        event.shaped(recipe.output, recipe.patterns, recipe.inputs).id(recipe.id);
     });
-
-    event
-        .shaped('4x appliedenergistics2:speed_card', ['AGA', 'GLG', 'AGA'], {
-            A: 'appliedenergistics2:advanced_card',
-            G: 'pneumaticcraft:glycerol',
-            L: {
-                type: 'pneumaticcraft:fluid',
-                tag: 'forge:lubricant',
-                amount: 1000
-            }
-        })
-        .id(id_prefix + 'speed_card');
 });
