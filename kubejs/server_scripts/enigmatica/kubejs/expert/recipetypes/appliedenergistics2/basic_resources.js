a7ba0483e103f91b3677d70028dfd24594d85ee5
ZZZank <3410764033@qq.com>
2023-07-11 20:31:53 +0800

    move misc recipes, add alt recipe for drive

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/appliedenergistics2/basic_resources.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/appliedenergistics2/basic_resources.js
@@ -15,11 +15,7 @@ onEvent('recipes', (event) => {
         { toReplace: 'appliedenergistics2:fluix_dust', replaceWith: 'botania:corporea_spark' },
         { toReplace: 'appliedenergistics2:certus_quartz_dust', replaceWith: 'botania:quartz_mana' }
     ];
-    recipes.forEach((recipe) => {
-        event.replaceInput({ mod: 'appliedenergistics2' }, recipe.toReplace, recipe.replaceWith);
-        event.replaceInput({ mod: 'aeadditions' }, recipe.toReplace, recipe.replaceWith);
-    });
-
+    
     const dusts = [
         {
             toReplace: 'appliedenergistics2:nether_quartz_dust',
@@ -28,25 +24,13 @@ onEvent('recipes', (event) => {
         { toReplace: 'appliedenergistics2:iron_dust', replaceWith: 'emendatusenigmatica:iron_dust' },
         { toReplace: 'appliedenergistics2:gold_dust', replaceWith: 'emendatusenigmatica:gold_dust' }
     ];
+
+    recipes.forEach((recipe) => {
+        event.replaceInput({ mod: 'appliedenergistics2' }, recipe.toReplace, recipe.replaceWith);
+        event.replaceInput({ mod: 'aeadditions' }, recipe.toReplace, recipe.replaceWith);
+    });
+
     dusts.forEach((entry) => {
         event.replaceInput(entry.toReplace, entry.replaceWith);
     });
-
-    event.recipes.create
-        .compacting('appliedenergistics2:fluix_pearl', [
-            'minecraft:ender_pearl',
-            'occultism:spirit_attuned_gem',
-            'occultism:spirit_attuned_gem',
-            'kubejs:dimensional_storage_crystal'
-        ])
-        .heated()
-        .id('appliedenergistics2:misc/fluixpearl');
-
-    event
-        .custom({
-            type: 'occultism:spirit_fire',
-            ingredient: Ingredient.of('quark:black_crystal').toJson(),
-            result: Ingredient.of('appliedenergistics2:sky_stone_block').toJson()
-        })
-        .id(id_prefix + 'sky_stone');
 });
