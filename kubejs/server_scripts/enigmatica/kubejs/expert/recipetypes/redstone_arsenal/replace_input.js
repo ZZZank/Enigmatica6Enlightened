22c7c0488087fab14ff0b3f9b1dbbe5b716f1d8f
ZZZank <3410764033@qq.com>
2023-07-15 17:12:04 +0800

    some rough scripts for Redstone Arsenal

---
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/redstone_arsenal/replace_input.js
@@ -0,0 +1,37 @@
+onEvent('recipes', (event) => {
+    if (global.isExpertMode == false) {
+        return;
+    }
+
+    const recipes = [
+        {
+            toReplace: 'redstone_arsenal:obsidian_rod',
+            replaceWith: 'immersiveengineering:graphite_electrode'
+        },
+        {
+            filter: {
+                not: [{ id: 'redstone_arsenal:storage/flux_metal_block' }]
+            },
+            toReplace: 'redstone_arsenal:flux_ingot',
+            replaceWith: 'powah:capacitor_blazing'
+        },
+        {
+            filter: {
+                not: [{ id: 'redstone_arsenal:storage/flux_gem_block' }]
+            },
+            toReplace: 'redstone_arsenal:flux_gem',
+            replaceWith: 'powah:crystal_nitro'
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        let filter = {
+            mod: 'redstone_arsenal'
+        };
+        if (recipe.filter) {
+            recipe.filter.mod = 'redstone_arsenal';
+            filter = recipe.filter;
+        }
+        event.replaceInput(filter, recipe.toReplace, recipe.replaceWith);
+    });
+});
