624f9813eae84eba92059bda9af7a76548170a0d
ZZZank <3410764033@qq.com>
2023-08-02 16:39:28 +0800

    temp conversion recipe, from arcane_gold to tainted_gold

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/shapeless.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/naturesaura/shapeless.js
@@ -2,12 +2,18 @@ onEvent('recipes', (event) => {
     if (global.isExpertMode == false) {
         return;
     }
+    const id_prefix = 'enigmatica:expert/naturesaura/shapeless/';
 
     const recipes = [
         {
             output: 'naturesaura:bottle_two_the_rebottling',
             inputs: ['minecraft:glass_bottle', 'farmersdelight:tree_bark'],
             id: 'naturesaura:bottle_two_the_rebottling'
+        },
+        {
+            output: 'naturesaura:altar/tainted_gold',
+            inputs: ['#forge:ingots/arcane_gold'],
+            id: `${id_prefix}arcane_gold_to_tainted_gold`
         }
     ];
 
