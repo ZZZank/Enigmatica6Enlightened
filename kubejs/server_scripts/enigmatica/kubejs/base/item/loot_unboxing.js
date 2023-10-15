760566b5872a9e8251c17cb88c9b85198b87e024
ZZZank <3410764033@qq.com>
2023-10-15 22:39:08 +0800

    add unboxing of actual lootbox item

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/item/loot_unboxing.js
@@ -0,0 +1,30 @@
+onEvent('item.right_click', (event) => {
+    let id = event.player.mainHandItem.id;
+    if (!id.startsWith('kubejs:') || !id.endsWith('_lootbox')) {
+        return;
+    }
+
+    let rarity = id.substr(7).replace('_lootbox', '');
+    let mod = event.player.mainHandItem.nbt ? event.player.mainHandItem.nbt.mod.toString() : '';
+    let lootTable = `enigmatica:chests/quest_${mod}_loot_${rarity}`; // ('kubejs:'.length) is 7
+
+    /*
+      // This piece of code is disabled, baceuse if anyone let a fake player to open such
+      // an item, it must be intended. 
+      // Also, 'event.player' can provide protections against unintended entity. 
+      if (!event.player.isPlayer() || event.player.isFake()) {
+          //kludge until a better handler is available to drop the item at the fake player's location.
+          let playerCoords = `${event.player.x} ${event.player.y + 1} ${event.player.z}`;
+          event.world.server.runCommand(
+              `/execute positioned ${playerCoords} run loot spawn ${playerCoords} loot ${lootTable}`
+          );
+          return;
+      }
+      */
+
+    let name = event.player.name;
+    event.server.runCommandSilent(`/execute at ${name} run loot give ${name} loot ${lootTable}`);
+
+    // not handling 'gamemode creative' case, due to performance and rarity of such a case
+    event.player.mainHandItem.count--;
+});
