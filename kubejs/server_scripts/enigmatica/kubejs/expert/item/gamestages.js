2f02c961f8db3c652c77f782a9f1314c73623abf
ZZZank <3410764033@qq.com>
2023-10-16 19:01:42 +0800

    tweak script for performance and visibility

--- kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
@@ -1,27 +1,22 @@
 onEvent('item.right_click', (event) => {
     const player = event.player;
 
-    let items = [
-        'kubejs:medium_machinery_schematics',
-        'kubejs:heavy_machinery_schematics',
-        'occultism:chalk_red',
-        'bloodmagic:masterbloodorb',
-        'bloodmagic:soulgemlesser'
-    ];
+    let stageMap = {
+        // item : stage ,
+        'kubejs:medium_machinery_schematics': 'medium_machinery_schematics',
+        'kubejs:heavy_machinery_schematics': 'heavy_machinery_schematics',
+        'occultism:chalk_red': 'red_chalk',
+        'bloodmagic:masterbloodorb': 'master_blood_orb',
+        'bloodmagic:soulgemlesser': 'lesser_tartaric_gem'
+    };
 
-    items.forEach((item) => {
-        if (player.mainHandItem == `${item}`) {
-            let stage_name = item.split(':')[1];
-            if (stage_name == 'masterbloodorb') stage_name = 'master_blood_orb';
-            if (stage_name == 'soulgemlesser') stage_name = 'lesser_tartaric_gem';
-            if (stage_name == 'chalk_red') stage_name = 'red_chalk';
-
-            if (!event.getEntity().stages.has(`${stage_name}`)) {
-                event.player.tell(`Gamestage Granted: ${titleCase(stage_name.replace(/_+/g, ' '))}`);
-                event.getEntity().stages.add(`${stage_name}`);
-            }
+    if (stageMap[player.mainHandItem]) {
+        const stage_name = stageMap[player.mainHandItem];
+        if (!event.getEntity().stages.has(`${stage_name}`)) {
+            event.player.tell(`Gamestage Granted: ${titleCase(stage_name.replace(/_+/g, ' '))}`);
+            event.getEntity().stages.add(`${stage_name}`);
         }
-    });
+    }
 });
 
 function titleCase(str) {
