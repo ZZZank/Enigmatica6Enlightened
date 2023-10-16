1f8fc0220f248217677b131c4df3a73409937faa
ZZZank <3410764033@qq.com>
2023-10-16 19:53:44 +0800

    rebuild soggy_treasure_box unboxing

--- kubejs/server_scripts/enigmatica/kubejs/base/item/fishing_treasure.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/item/fishing_treasure.js
@@ -1,21 +1,16 @@
 onEvent('item.right_click', (e) => {
-    if (e.item.id != 'kubejs:soggy_treasure_box') {
+    const player = e.player;
+    if (player.mainHandItem.id != 'kubejs:soggy_treasure_box') {
         return;
     }
-    if (!e.player.isCreativeMode()) {
-        e.player.getMainHandItem().count--;
-    }
-    let lootTable = 'enigmatica:chests/soggy_treasure_box';
 
-    if (!e.player.isPlayer() || e.player.isFake()) {
-        //kludge until a better handler is available to drop the item at the fake player's location.
-        let playerCoords = `${e.player.x} ${e.player.y + 1} ${e.player.z}`;
-        e.world.server.runCommand(
-            `/execute positioned ${playerCoords} run loot spawn ${playerCoords} loot ${lootTable}`
-        );
-    } else {
-        e.server.runCommandSilent(
-            `/execute at ${e.player.name} run loot give ${e.player.name} loot ${lootTable}`
-        );
-    }
+    player.mainHandItem.count--;
+
+    let lootTable = 'enigmatica:chests/soggy_treasure_box';
+    let playerCoords = `${player.x} ${player.y + 1} ${player.z}`;
+
+    // universial solutions for both fake and real player
+    e.server.runCommandSilent(
+        `/execute positioned ${playerCoords} run loot spawn ${playerCoords} loot ${lootTable}`
+    );
 });
