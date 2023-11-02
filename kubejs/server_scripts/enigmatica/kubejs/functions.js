1920789866fa75a2649871664737acb27bfef0b2
ZZZank <3410764033@qq.com>
2023-11-02 21:12:11 +0800

    switch recipe id getter, again

--- kubejs/server_scripts/enigmatica/kubejs/functions.js
+++ kubejs/server_scripts/enigmatica/kubejs/functions.js
@@ -111,7 +111,7 @@ function lowerTiers(tiers, tier) {
 
 // transplant the md5 from `<type's mod>:kjs_<hash>` onto the supplied prefix
 function fallback_id(recipe, id_prefix) {
-    if (recipe.id.path.startsWith('kjs_')) {
+    if (recipe.id.path.includes('kjs_')) {
         recipe.serializeJson(); // without this the hashes *will* collide
         recipe.id(id_prefix + 'md5_' + recipe.getUniqueId());
     }
