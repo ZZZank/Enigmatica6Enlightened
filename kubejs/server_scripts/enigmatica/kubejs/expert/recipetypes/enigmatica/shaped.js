9c49833518c6158dff248acfb5939372ad04676f
ZZZank <3410764033@qq.com>
2023-11-04 20:02:44 +0800

    rework Storage Drawers recipe

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/enigmatica/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/enigmatica/shaped.js
@@ -6,88 +6,84 @@ onEvent('recipes', (event) => {
     const id_prefix = 'enigmatica:expert/enigmatica/';
     const recipes = [];
 
-    buildWoodVariants.forEach((wood) => {
-        if (wood.modId == 'minecraft') {
+    let woodTypeDupes = [
+        'palo_verde',
+        'withering_oak',
+        'blue_archwood',
+        'green_archwood',
+        'purple_archwood',
+        'menril_filled',
+        'watchful_aspen',
+        'crustose',
+        'sappy_maple',
+        'avocado'
+    ];
+    let acceptedPlank = [];
+    let acceptedSlab = [];
+    buildWoodVariants.forEach((variant) => {
+        if (variant.modId == 'minecraft' || woodTypeDupes.includes(variant.logType)) {
             return;
         }
-
-        let dupes = [
-            'palo_verde',
-            'withering_oak',
-            'blue_archwood',
-            'green_archwood',
-            'purple_archwood',
-            'menril_filled',
-            'watchful_aspen',
-            'crustose',
-            'sappy_maple',
-            'avocado'
-        ];
-
-        if (dupes.includes(wood.logType)) {
-            return;
-        }
-
-        //All recipes using planks here
-
-        recipes.push(
-            {
-                output: Item.of('storagedrawers:oak_full_drawers_1'),
-                pattern: ['AAA', ' C ', 'AAA'],
-                key: {
-                    A: wood.plankBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_full_drawers_1_from_${wood.logType}_planks`
-            },
-            {
-                output: Item.of('storagedrawers:oak_full_drawers_2', 2),
-                pattern: ['ACA', 'AAA', 'ACA'],
-                key: {
-                    A: wood.plankBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_full_drawers_2_from_${wood.logType}_planks`
-            },
-            {
-                output: Item.of('storagedrawers:oak_full_drawers_4', 4),
-                pattern: ['CAC', 'AAA', 'CAC'],
-                key: {
-                    A: wood.plankBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_full_drawers_4_from_${wood.logType}_planks`
-            },
-
-            {
-                output: Item.of('storagedrawers:oak_half_drawers_1'),
-                pattern: ['AAA', ' C ', 'AAA'],
-                key: {
-                    A: wood.slabBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_half_drawers_1_from_${wood.logType}_slab`
-            },
-            {
-                output: Item.of('storagedrawers:oak_half_drawers_2', 2),
-                pattern: ['ACA', 'AAA', 'ACA'],
-                key: {
-                    A: wood.slabBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_half_drawers_2_from_${wood.logType}_slab`
-            },
-            {
-                output: Item.of('storagedrawers:oak_half_drawers_4', 4),
-                pattern: ['CAC', 'AAA', 'CAC'],
-                key: {
-                    A: wood.slabBlock,
-                    C: 'farmersdelight:basket'
-                },
-                id: `${id_prefix}oak_half_drawers_4_from_${wood.logType}_slab`
-            }
-        );
+        acceptedPlank.push(variant.plankBlock);
+        acceptedSlab.push(variant.slabBlock);
     });
+    recipes.push(
+        {
+            output: Item.of('storagedrawers:oak_full_drawers_1'),
+            pattern: ['AAA', ' C ', 'AAA'],
+            key: {
+                A: acceptedPlank,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_full_drawers_1_from_unvanilla_planks`
+        },
+        {
+            output: Item.of('storagedrawers:oak_full_drawers_2', 2),
+            pattern: ['ACA', 'AAA', 'ACA'],
+            key: {
+                A: acceptedPlank,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_full_drawers_2_from_unvanilla_planks`
+        },
+        {
+            output: Item.of('storagedrawers:oak_full_drawers_4', 4),
+            pattern: ['CAC', 'AAA', 'CAC'],
+            key: {
+                A: acceptedPlank,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_full_drawers_4_from_unvanilla_planks`
+        },
+
+        {
+            output: Item.of('storagedrawers:oak_half_drawers_1'),
+            pattern: ['AAA', ' C ', 'AAA'],
+            key: {
+                A: acceptedSlab,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_half_drawers_1_from_unvanilla_slab`
+        },
+        {
+            output: Item.of('storagedrawers:oak_half_drawers_2', 2),
+            pattern: ['ACA', 'AAA', 'ACA'],
+            key: {
+                A: acceptedSlab,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_half_drawers_2_from_unvanilla_slab`
+        },
+        {
+            output: Item.of('storagedrawers:oak_half_drawers_4', 4),
+            pattern: ['CAC', 'AAA', 'CAC'],
+            key: {
+                A: acceptedSlab,
+                C: 'farmersdelight:basket'
+            },
+            id: `${id_prefix}oak_half_drawers_4_from_unvanilla_slab`
+        }
+    );
 
     recipes.forEach((recipe) => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
