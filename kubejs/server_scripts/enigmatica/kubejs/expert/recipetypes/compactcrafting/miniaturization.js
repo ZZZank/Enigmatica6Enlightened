a74e259d0e513a42e542ad21a8d28c3aba469140
ZZZank <3410764033@qq.com>
2023-11-13 16:40:55 +0800

    reconstruct compact crafting script

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/compactcrafting/miniaturization.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/compactcrafting/miniaturization.js
@@ -4,60 +4,14 @@ onEvent('recipes', (event) => {
 
     //Also note, can't use Item.of because Count is caps sensitive (Name too)
 
-    const recipes = [];
-
-    let tiers = [
-        ['tiny', 'resourcefulbees:forest_honeycomb_block'],
-        ['small', 'resourcefulbees:aluminum_honeycomb_block'],
-        ['normal', 'resourcefulbees:zinc_honeycomb_block'],
-        ['large', 'resourcefulbees:uranium_honeycomb_block'],
-        ['giant', 'resourcefulbees:cobalt_honeycomb_block'],
-        ['maximum', 'resourcefulbees:industrious_honeycomb_block']
-    ];
-
-    tiers.forEach(([tier, craftBlock]) => {
-        recipes.push({
+    const recipes = [
+        /*
+        {
             recipeSize: 5,
             layers: [
                 {
                     type: 'compactcrafting:mixed',
-                    pattern: [
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', 'W', 'E', 'W', 'W'],
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', 'W', 'W', 'W', 'W']
-                    ]
-                },
-                {
-                    type: 'compactcrafting:mixed',
-                    pattern: [
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', ' ', ' ', ' ', 'W'],
-                        ['W', ' ', 'H', ' ', 'W'],
-                        ['W', ' ', ' ', ' ', 'W'],
-                        ['W', 'W', 'W', 'W', 'W']
-                    ]
-                },
-                {
-                    type: 'compactcrafting:mixed',
-                    pattern: [
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', ' ', 'H', ' ', 'W'],
-                        ['W', 'H', 'T', 'H', 'W'],
-                        ['W', ' ', 'H', ' ', 'W'],
-                        ['W', 'W', 'W', 'W', 'W']
-                    ]
-                },
-                {
-                    type: 'compactcrafting:mixed',
-                    pattern: [
-                        ['W', 'W', 'W', 'W', 'W'],
-                        ['W', ' ', ' ', ' ', 'W'],
-                        ['W', ' ', 'H', ' ', 'W'],
-                        ['W', ' ', ' ', ' ', 'W'],
-                        ['W', 'W', 'W', 'W', 'W']
-                    ]
+                    pattern: ['WWWWW', 'WWWWW', 'WWEWW', 'WWWWW', 'WWWWW']
                 },
                 {
                     type: 'compactcrafting:filled',
@@ -69,7 +23,73 @@ onEvent('recipes', (event) => {
                 Count: 1
             },
             components: {
-                E:{
+                E: {
+                    type: 'compactcrafting:block',
+                    block: 'rftoolsutility:matter_transmitter'
+                },
+                W: {
+                    type: 'compactcrafting:block',
+                    block: 'compactmachines:wall'
+                },
+                H: {
+                    type: 'compactcrafting:block',
+                    block: craftBlock
+                },
+                T: {
+                    type: 'compactcrafting:block',
+                    block: 'mekanism:teleporter'
+                }
+            },
+            outputs: [
+                {
+                    id: `compactmachines:machine_${tier}`,
+                    Count: 1
+                }
+            ]
+        }
+        */
+    ];
+
+    let compactMachines = [
+        ['tiny', 'resourcefulbees:forest_honeycomb_block'],
+        ['small', 'resourcefulbees:aluminum_honeycomb_block'],
+        ['normal', 'resourcefulbees:zinc_honeycomb_block'],
+        ['large', 'resourcefulbees:uranium_honeycomb_block'],
+        ['giant', 'resourcefulbees:cobalt_honeycomb_block'],
+        ['maximum', 'resourcefulbees:industrious_honeycomb_block']
+    ];
+
+    compactMachines.forEach(([tier, craftBlock]) => {
+        recipes.push({
+            recipeSize: 5,
+            layers: [
+                {
+                    type: 'compactcrafting:mixed',
+                    pattern: ['WWWWW', 'WWWWW', 'WWEWW', 'WWWWW', 'WWWWW']
+                },
+                {
+                    type: 'compactcrafting:mixed',
+                    pattern: ['WWWWW', 'W   W', 'W H W', 'W   W', 'WWWWW']
+                },
+                {
+                    type: 'compactcrafting:mixed',
+                    pattern: ['WWWWW', 'W H W', 'WHTHW', 'W H W', 'WWWWW']
+                },
+                {
+                    type: 'compactcrafting:mixed',
+                    pattern: ['WWWWW', 'W   W', 'W H W', 'W   W', 'WWWWW']
+                },
+                {
+                    type: 'compactcrafting:filled',
+                    component: 'W'
+                }
+            ],
+            catalyst: {
+                id: 'thermal:upgrade_augment_1',
+                Count: 1
+            },
+            components: {
+                E: {
                     type: 'compactcrafting:block',
                     block: 'rftoolsutility:matter_transmitter'
                 },
@@ -96,6 +116,14 @@ onEvent('recipes', (event) => {
     });
 
     recipes.forEach((recipe) => {
+        recipe.layers.forEach((layer) => {
+            if (layer.type != 'compactcrafting:mixed') {
+                return;
+            }
+            for (let i = layer.pattern.length; i >= 0; i++) {
+                layer.pattern[i] = layer.pattern[i].split('');
+            }
+        });
         recipe.type = 'compactcrafting:miniaturization';
         event.custom(recipe);
     });
