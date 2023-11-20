30b731cbbea22d6cf1d97988255cd92bf41b658b
ZZZank <3410764033@qq.com>
2023-11-20 20:55:53 +0800

    remove the needs of manually setting modpackUrl
    by bundle modpack file inside serverpack

--- automation/modpack-uploader.ps1
+++ automation/modpack-uploader.ps1
@@ -289,11 +289,17 @@ function New-ServerFiles {
         [int]$ClientFileReturnId
     )
     if ($ENABLE_SERVER_FILE_MODULE) {
+        $clientZip = "$CLIENT_ZIP_NAME.zip"
         $serverZip = "$SERVER_ZIP_NAME.zip"
         Remove-Item $serverZip -Force -ErrorAction SilentlyContinue
         Write-Host 
         Write-Host "Creating server files..." -ForegroundColor Cyan
         Write-Host 
+        if ($uploadExpertMode) {
+            Copy-Item -Path $clientZip -Destination "server_files_expert\mainpack.zip" -ErrorAction SilentlyContinue
+        }else {
+            Copy-Item -Path $clientZip -Destination "server_files\mainpack.zip" -ErrorAction SilentlyContinue
+        }
         7z a -tzip $serverZip "$SERVER_FILES_FOLDER\*"
         Move-Item -Path "automation\$serverZip" -Destination $serverZip -ErrorAction SilentlyContinue
         Write-Host "Server files created!" -ForegroundColor Green
