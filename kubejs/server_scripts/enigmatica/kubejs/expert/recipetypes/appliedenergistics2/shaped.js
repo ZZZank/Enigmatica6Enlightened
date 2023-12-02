2e2e3e1c5cd00f771dd1646542575244401bd422
ZZZank <3410764033@qq.com>
2023-12-02 18:29:51 +0800

    move AE shaped recipe

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/appliedenergistics2/shaped.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/appliedenergistics2/shaped.js
@@ -7,6 +7,54 @@ onEvent('recipes', (event) => {
 
     const recipes = [
         {
+            output: 'appliedenergistics2:2_cubed_spatial_cell_component',
+            patterns: ['GFG', 'FAF', ' S '],
+            inputs: {
+                G: 'minecraft:glowstone_dust',
+                A: 'appliedenergistics2:engineering_processor',
+                F: 'appliedenergistics2:fluix_pearl',
+                S: 'occultism:spirit_attuned_gem'
+            },
+            id: 'appliedenergistics2:network/cells/spatial_components'
+        },
+        {
+            output: 'appliedenergistics2:memory_card',
+            patterns: ['   ', 'VMQ', 'GGG'],
+            inputs: {
+                V: 'integrateddynamics:variable',
+                M: 'kubejs:memory_basic_filled',
+                Q: 'pneumaticcraft:ingot_iron_compressed',
+                G: 'minecraft:gold_ingot'
+            },
+            id: 'appliedenergistics2:tools/network_memory_card'
+        },
+        {
+            output: 'appliedenergistics2:blank_pattern',
+            patterns: ['QGQ', 'GAG', 'RSR'],
+            inputs: {
+                S: 'create:empty_schematic',
+                Q: 'appliedenergistics2:quartz_glass',
+                G: 'minecraft:glowstone_dust',
+                R: 'pneumaticcraft:ingot_iron_compressed',
+                A: '#forge:gems/aquamarine'
+            },
+            id: 'appliedenergistics2:network/crafting/patterns_blank'
+        },
+        {
+            output: '4x appliedenergistics2:speed_card',
+            patterns: ['AGA', 'GLG', 'AGA'],
+            inputs: {
+                A: 'appliedenergistics2:advanced_card',
+                G: 'pneumaticcraft:glycerol',
+                L: {
+                    type: 'pneumaticcraft:fluid',
+                    tag: 'forge:lubricant',
+                    amount: 1000
+                }
+            },
+            id: 'appliedenergistics2:materials/cardspeed'
+        },
+        {
             output: 'appliedenergistics2:interface',
             patterns: ['EAE', 'DSC', 'EAE'],
             inputs: {
@@ -48,40 +96,6 @@ onEvent('recipes', (event) => {
                 S: 'appliedenergistics2:sky_dust'
             },
             id: 'appliedenergistics2:network/blocks/storage_drive'
-        },
-        {
-            output: 'appliedenergistics2:storage_bus',
-            patterns: ['SC ', 'IB ', 'SD '],
-            inputs: {
-                B: '#xnet:connectors',
-                C: 'appliedenergistics2:formation_core',
-                I: 'appliedenergistics2:logic_processor',
-                D: 'appliedenergistics2:annihilation_core',
-                S: 'occultism:spirit_attuned_gem'
-            },
-            id: 'appliedenergistics2:network/parts/storage_bus'
-        },
-        {
-            output: 'appliedenergistics2:import_bus',
-            patterns: ['SD ', 'IC ', 'SD '],
-            inputs: {
-                D: 'appliedenergistics2:annihilation_core',
-                I: 'appliedenergistics2:logic_processor',
-                S: 'occultism:spirit_attuned_gem',
-                C: '#xnet:connectors'
-            },
-            id: 'appliedenergistics2:network/parts/import_bus'
-        },
-        {
-            output: 'appliedenergistics2:export_bus',
-            patterns: ['SD ', 'IC ', 'SD '],
-            inputs: {
-                D: 'appliedenergistics2:formation_core',
-                I: 'appliedenergistics2:logic_processor',
-                S: 'occultism:spirit_attuned_gem',
-                C: '#xnet:connectors'
-            },
-            id: 'appliedenergistics2:network/parts/export_bus'
         },
         {
             output: 'appliedenergistics2:spatial_pylon',
