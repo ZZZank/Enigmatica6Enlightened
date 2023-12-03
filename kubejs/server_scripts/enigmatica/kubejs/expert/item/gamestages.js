8673e2722ab0fc58f3a35aaf775c0c729f78818c
ZZZank <3410764033@qq.com>
2023-12-03 19:53:45 +0800

    prevent accessing `stageMap` from outside

--- kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/item/gamestages.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const stageMap = {
-    // item : stage ,
-    'kubejs:medium_machinery_schematics': 'medium_machinery_schematics',
-    'kubejs:heavy_machinery_schematics': 'heavy_machinery_schematics',
-    'occultism:chalk_red': 'red_chalk',
-    'bloodmagic:masterbloodorb': 'master_blood_orb',
-    'bloodmagic:soulgemlesser': 'lesser_tartaric_gem'
-};
+{
+    const stageMap = {
+        // item : stage ,
+        'kubejs:medium_machinery_schematics': 'medium_machinery_schematics',
+        'kubejs:heavy_machinery_schematics': 'heavy_machinery_schematics',
+        'occultism:chalk_red': 'red_chalk',
+        'bloodmagic:masterbloodorb': 'master_blood_orb',
+        'bloodmagic:soulgemlesser': 'lesser_tartaric_gem'
+    };
 
-onEvent('item.right_click', (event) => {
-    const /** @type {Internal.PlayerJS} */ player = event.player;
-    if (!player || !player.player || player.fake || player.mainHandItem.empty) {
-        return;
-    }
+    onEvent('item.right_click', (event) => {
+        const /** @type {Internal.PlayerJS} */ player = event.player;
+        if (!player || !player.player || player.fake || player.mainHandItem.empty) {
+            return;
+        }
 
-    const stage_name = stageMap[player.mainHandItem];
-    if (stage_name && !player.stages.has(`${stage_name}`)) {
-        player.tell(`Gamestage Granted: ${titleCase(stage_name.replace(/_+/g, ' '))}`);
-        player.stages.add(`${stage_name}`);
-    }
-});
+        const stage_name = stageMap[player.mainHandItem];
+        if (stage_name && !player.stages.has(stage_name)) {
+            player.tell(`Gamestage Granted: ${titleCase(stage_name.replace(/_+/g, ' '))}`);
+            player.stages.add(`${stage_name}`);
+        }
+    });
+}
