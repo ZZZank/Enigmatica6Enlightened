f0e62ddfc3ceee9b687bab8704abed1339ff2443
ZZZank <3410764033@qq.com>
2023-12-06 11:07:41 +0800

    hide machine_structure_killing_entity

--- kubejs/client_scripts/item_modifiers/jei_remove_categories.js
+++ kubejs/client_scripts/item_modifiers/jei_remove_categories.js
@@ -4,9 +4,11 @@ onEvent('jei.remove.categories', (event) => {
     // console.log('JEI RECIPE CATEGORIES: ' + event.getCategoryIds());
 
     let categories = [
-        'masterfulmachinery:machine_structure_recipe_hint_right_click_block'
-    ]
-    for (let category of categories) {
+        'masterfulmachinery:machine_structure_recipe_hint_right_click_block',
+        'masterfulmachinery:machine_structure_killing_entity'
+    ];
+
+    categories.forEach((category) => {
         event.remove(category);
-    }
+    });
 });
