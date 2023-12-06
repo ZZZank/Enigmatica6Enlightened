af9aebcd91696f21ef2f496fe9e0346b1d7a9c02
ZZZank <3410764033@qq.com>
2023-12-06 20:41:46 +0800

    config auto update

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/item/tmp.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// onEvent('item.right_click', (e) => {
+//     const /** @type {Internal.PlayerJS} */ player = e.player;
+//     if (player.fake || player.offHandItem.id != 'minecraft:stick' || !player.creativeMode) {
+//         return;
+//     }
+
+//     let str = Ingredient.of('3x #forge:grain').toJson()
+
+//     player.tell(str.count)
+//     player.tell(str);
+// });
