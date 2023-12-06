b4614bc795fe9e20104a43a9cfe3ef86f252708c
ZZZank <3410764033@qq.com>
2023-12-06 19:33:34 +0800

    unify compressed_iron creating in normal mode
    use interactio to handle it

---
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipetypes/interactio/item_explode.js
@@ -0,0 +1,38 @@
+'use strict';
+
+onEvent('recipes', (event) => {
+    if (global.isNormalMode == false) {
+        return;
+    }
+    const id_prefix = 'enigmatica:normal/interactio/item_explode/';
+    const recipes = [
+        {
+            inputs: [{ tag: 'forge:storage_blocks/iron', count: 1 }],
+            output: {
+                entries: [{ result: { item: 'pneumaticcraft:compressed_iron_block', count: 1 }, weight: 4 }],
+                empty_weight: 1,
+                rolls: 1
+            },
+            id: 'pneumaticcraft:explosion_crafting/compressed_iron_block'
+        },
+        {
+            inputs: [{ tag: 'forge:ingots/iron', count: 1 }],
+            output: {
+                entries: [{ result: { item: 'pneumaticcraft:compressed_iron_ingot', count: 1 }, weight: 4 }],
+                empty_weight: 1,
+                rolls: 1
+            },
+            id: 'pneumaticcraft:explosion_crafting/compressed_iron_ingot'
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event
+            .custom({
+                type: 'interactio:item_explode',
+                inputs: recipe.inputs,
+                output: recipe.output
+            })
+            .id(recipe.id);
+    });
+});
