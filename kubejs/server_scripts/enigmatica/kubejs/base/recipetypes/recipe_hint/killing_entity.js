cdc3753e088371dae6e51f5d3b239fb3f0da70bb
ZZZank <3410764033@qq.com>
2023-12-12 22:46:58 +0800

    more recipe hint for killing_entity

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/recipe_hint/killing_entity.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/recipe_hint/killing_entity.js
@@ -2,31 +2,55 @@ onEvent('recipes', (event) => {
     const id_prefix = 'enlightened6:killing_entity/';
     const recipes = [
         {
+            target: 'minecraft:ravager_spawn_egg',
+            output: ['quark:ravager_hide'],
+            id: id_prefix + 'ravager'
+        },
+        {
+            target: 'minecraft:wither_skeleton_spawn_egg',
+            output: [
+                'minecraft:wither_skeleton_skull',
+                'architects_palette:withered_bone',
+                'wstweaks:fragment'
+            ],
+            id: id_prefix + 'wither_skeleton'
+        },
+        {
+            target: 'minecraft:elder_guardian_spawn_egg',
+            output: ['upgrade_aquatic:elder_guardian_spine', 'upgrade_aquatic:elder_eye'],
+            id: id_prefix + 'elder_guardian'
+        },
+        {
+            target: 'minecraft:guardian_spawn_egg',
+            output: ['upgrade_aquatic:guardian_spine'],
+            id: id_prefix + 'guardian'
+        },
+        {
             target: 'minecraft:pig_spawn_egg',
             weapon: '#farmersdelight:tools/knives',
             output: ['farmersdelight:ham'],
-            id: id_prefix + 'ham_pig'
+            id: id_prefix + 'pig'
         },
         {
             target: 'minecraft:hoglin_spawn_egg',
             weapon: '#farmersdelight:tools/knives',
-            output: ['farmersdelight:ham'],
-            id: id_prefix + 'ham_hoglin'
+            output: ['farmersdelight:ham', 'nethers_delight:hoglin_hide'],
+            id: id_prefix + 'hoglin'
         },
         {
             target: 'upgrade_aquatic:nautilus_spawn_egg',
             output: ['minecraft:nautilus_shell'],
-            id: id_prefix + 'nautilus_shell'
+            id: id_prefix + 'nautilus'
         },
         {
             target: 'ars_nouveau:ritual_wilden_summon',
             output: ['ars_nouveau:wilden_tribute'],
-            id: id_prefix + 'wilden_tribute'
+            id: id_prefix + 'wilden_summon'
         },
         {
             target: 'betterendforge:silk_moth_nest',
             output: ['betterendforge:silk_fiber'],
-            id: id_prefix + 'silk_fiber'
+            id: id_prefix + 'silk_moth'
         },
         {
             target: 'meetyourfight:haunted_bell',
