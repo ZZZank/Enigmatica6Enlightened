d9a9342d05940e4476e78a00dcadab2b1f5aaf6f
ZZZank <3410764033@qq.com>
2023-12-13 11:10:01 +0800

    Extra Storage compat

---
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/compat/extrastorage/misc.js
@@ -0,0 +1,23 @@
+'use strict';
+onEvent('recipes', (event) => {
+    if (global.isNormalMode || !Platform.isLoaded('extrastorage')) {
+        return;
+    }
+    const id_prefix = 'enigmatica:expert/extrastorage/misc/';
+    const recipes = [];
+
+    event
+        .custom({
+            type: 'industrialforegoing:dissolution_chamber',
+            input: ['extrastorage:diamond_crafter'].map((input) => Ingredient.of(input)),
+            inputFluid: `{FluidName:"${'tconstruct:molten_netherite'}",Amount:${144 * 9}}`,
+            processingTime: 200,
+            output: toJsonWithCount('extrastorage:netherite_crafter'),
+            outputFluid: `{FluidName:"${''}",Amount:${0}}`
+        })
+        .id('extrastorage:netherite_crafter');
+
+    recipes.forEach((recipe) => {
+        event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
+    });
+});
