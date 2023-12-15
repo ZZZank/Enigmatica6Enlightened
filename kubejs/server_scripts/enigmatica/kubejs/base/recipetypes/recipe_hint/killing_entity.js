71bc4b6620f2be39bc8bdcacdc44d13050580af1
ZZZank <3410764033@qq.com>
2023-12-15 21:05:29 +0800

    makes killing_entity supports multiple inputs

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/recipe_hint/killing_entity.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/recipe_hint/killing_entity.js
@@ -2,12 +2,12 @@ onEvent('recipes', (event) => {
     const id_prefix = 'enlightened6:killing_entity/';
     const recipes = [
         {
-            target: 'minecraft:ravager_spawn_egg',
+            target: ['minecraft:ravager_spawn_egg'],
             output: ['quark:ravager_hide'],
             id: id_prefix + 'ravager'
         },
         {
-            target: 'minecraft:wither_skeleton_spawn_egg',
+            target: ['minecraft:wither_skeleton_spawn_egg'],
             output: [
                 'minecraft:wither_skeleton_skull',
                 'architects_palette:withered_bone',
@@ -16,59 +16,59 @@ onEvent('recipes', (event) => {
             id: id_prefix + 'wither_skeleton'
         },
         {
-            target: 'minecraft:elder_guardian_spawn_egg',
+            target: ['minecraft:elder_guardian_spawn_egg'],
             output: ['upgrade_aquatic:elder_guardian_spine', 'upgrade_aquatic:elder_eye'],
             id: id_prefix + 'elder_guardian'
         },
         {
-            target: 'minecraft:guardian_spawn_egg',
+            target: ['minecraft:guardian_spawn_egg'],
             output: ['upgrade_aquatic:guardian_spine'],
             id: id_prefix + 'guardian'
         },
         {
-            target: 'minecraft:pig_spawn_egg',
+            target: ['minecraft:pig_spawn_egg'],
             weapon: '#farmersdelight:tools/knives',
             output: ['farmersdelight:ham'],
             id: id_prefix + 'pig'
         },
         {
-            target: 'minecraft:hoglin_spawn_egg',
+            target: ['minecraft:hoglin_spawn_egg'],
             weapon: '#farmersdelight:tools/knives',
             output: ['farmersdelight:ham', 'nethers_delight:hoglin_hide'],
             id: id_prefix + 'hoglin'
         },
         {
-            target: 'upgrade_aquatic:nautilus_spawn_egg',
+            target: ['upgrade_aquatic:nautilus_spawn_egg'],
             output: ['minecraft:nautilus_shell'],
             id: id_prefix + 'nautilus'
         },
         {
-            target: 'ars_nouveau:ritual_wilden_summon',
+            target: ['ars_nouveau:ritual_wilden_summon'],
             output: ['ars_nouveau:wilden_tribute'],
             id: id_prefix + 'wilden_summon'
         },
         {
-            target: 'betterendforge:silk_moth_nest',
+            target: ['betterendforge:silk_moth_nest'],
             output: ['betterendforge:silk_fiber'],
             id: id_prefix + 'silk_moth'
         },
         {
-            target: 'meetyourfight:haunted_bell',
+            target: ['meetyourfight:haunted_bell'],
             output: ['meetyourfight:phantoplasm'],
             id: id_prefix + 'phantoplasm'
         },
         {
-            target: 'meetyourfight:fossil_bait',
+            target: ['meetyourfight:fossil_bait'],
             output: ['meetyourfight:mossy_tooth'],
             id: id_prefix + 'mossy_tooth'
         },
         {
-            target: 'meetyourfight:devils_ante',
+            target: ['meetyourfight:devils_ante'],
             output: ['meetyourfight:fortunes_favor'],
             id: id_prefix + 'fortunes_favor'
         },
         {
-            target: 'environmental:koi_bucket',
+            target: ['environmental:koi_bucket'],
             output: ['bloodmagic:slate_ampoule', 'aquaculture:fish_bones'],
             id: id_prefix + 'koi'
         }
@@ -85,18 +85,19 @@ onEvent('recipes', (event) => {
                     data: toJsonWithCount(output)
                 };
             }),
-            inputs: [
-                {
+            inputs: recipe.target.map((input) => {
+                return {
                     type: 'masterfulmachinery:items',
-                    data: recipe.weapon ? toJsonWithCount(recipe.weapon) : { tag: 'forge:weapons', count: 1 }
-                },
-                {
-                    type: 'masterfulmachinery:items',
-                    data: toJsonWithCount(recipe.target)
-                }
-            ],
+                    data: toJsonWithCount(input)
+                };
+            }),
             ticks: 1
         };
+        proc.inputs.push({
+            type: 'masterfulmachinery:items',
+            data: toJsonWithCount(recipe.weapon ? recipe.weapon : '#forge:weapons')
+        });
+
         event.custom(proc).id(recipe.id);
     });
 });
