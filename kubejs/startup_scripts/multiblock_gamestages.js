22377945b8159e5811689ff56422dc862215ccc6
ZZZank <3410764033@qq.com>
2024-01-22 14:22:24 +0800

    fix lang key name

--- kubejs/startup_scripts/multiblock_gamestages.js
+++ kubejs/startup_scripts/multiblock_gamestages.js
@@ -32,7 +32,7 @@ onEvent('ie.multiblock.form', (event) => {
     if (stage && !event.getEntity().stages.has(stage)) {
         event.cancel();
         if (!event.getEntity().getServer()) {
-            let error_message = Text.translate('desc.enigmatica.base.ie_multiblock_error');
+            const error_message = Text.translate('desc.enigmatica.ie_multiblock_error');
             event.getEntity().tell(error_message);
         }
     }
