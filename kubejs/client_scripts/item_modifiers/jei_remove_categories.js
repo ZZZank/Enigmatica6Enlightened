7fffae7f0e75f2ff4468811bda1c3a72a3ff9adf
ZZZank <3410764033@qq.com>
2024-01-30 15:08:32 +0800

    remove unused category removals

--- kubejs/client_scripts/item_modifiers/jei_remove_categories.js
+++ kubejs/client_scripts/item_modifiers/jei_remove_categories.js
@@ -4,13 +4,8 @@ onEvent('jei.remove.categories', (event) => {
     // console.log('JEI RECIPE CATEGORIES: ' + event.getCategoryIds());
 
     let categories = [
-        'occultism:miner',
-        'masterfulmachinery:machine_structure_recipe_hint_general',
-        'masterfulmachinery:machine_structure_recipe_hint_right_click_block',
-        'masterfulmachinery:machine_structure_killing_entity'
+        'occultism:miner'
     ];
 
-    categories.forEach((category) => {
-        event.remove(category);
-    });
+    event.remove(categories);
 });
