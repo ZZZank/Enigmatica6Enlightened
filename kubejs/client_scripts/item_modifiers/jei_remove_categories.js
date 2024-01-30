55489dd3598221abfc6acfc54cebdf751c5e525f
ZZZank <3410764033@qq.com>
2024-01-30 15:14:50 +0800

    remove MM structure display in JEI
    players will Create or Building Gadgets anyway

--- kubejs/client_scripts/item_modifiers/jei_remove_categories.js
+++ kubejs/client_scripts/item_modifiers/jei_remove_categories.js
@@ -4,6 +4,13 @@ onEvent('jei.remove.categories', (event) => {
     // console.log('JEI RECIPE CATEGORIES: ' + event.getCategoryIds());
 
     let categories = [
+        'masterfulmachinery:machine_structure_auto_iridescent_altar',
+        'masterfulmachinery:machine_structure_advanced_assembly_table',
+        'masterfulmachinery:machine_structure_enigmatic_tree_of_life',
+        'masterfulmachinery:machine_structure_wicked_altar',
+        'masterfulmachinery:machine_structure_industrial_deuterium_plant',
+        'masterfulmachinery:machine_structure_gaia_reactor',
+        'masterfulmachinery:machine_structure_stellar_neutron_activator',
         'occultism:miner'
     ];
 
