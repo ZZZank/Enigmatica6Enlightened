68e79a0fb126c48cf2c07d54470b700c12f8d99f
ZZZank <3410764033@qq.com>
2024-03-02 14:24:06 +0800

    dev auto update

--- kubejs/server_scripts/enigmatica/kubejs/base/item/tmp.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/item/tmp.js
@@ -1,20 +1,17 @@
 'use strict';
 
-// onEvent('item.right_click', (e) => {
-//     const player = e.player;
-//     if (player.fake || player.offHandItem.id != 'minecraft:stick' || !player.creativeMode) {
-//         return;
-//     }
+onEvent('item.right_click', (e) => {
+    const player = e.player;
+    if (player.fake || player.offHandItem.id != 'minecraft:stick' || !player.creativeMode) {
+        return;
+    }
 
-//     let a = Item.of('pneumaticcraft:reinforced_chest', JSON.stringify({
-//         display: {
-//             Name: '[{"translate":"desc.enigmatica.amadron.atum_mineral"}]',
-//             Lore: ['[{"translate":"desc.enigmatica.amadron.atum_mineral.lore","color":"gold"}]']
-//         },
-//         BlockEntityTag: { LootTable: 'enigmatica:chests/amadron_mineral_survey_atum_combo' }
-//     }));
+    let a = [
+    ]
 
-//     player.give(a);
+    a.forEach(s => {
+        player.give(s)
+    });
 
-//     player.tell(a);
-// });
+    // player.tell(a);
+});
