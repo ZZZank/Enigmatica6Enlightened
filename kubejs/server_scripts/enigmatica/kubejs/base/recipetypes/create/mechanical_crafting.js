05599a9f8e505688fdeb25ef0d37ee647f01e7b6
ZZZank <3410764033@qq.com>
2024-03-26 23:53:44 +0800

    'astralsorcery:observatory', why can't I craft it using normal recipe?

--- kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/mechanical_crafting.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipetypes/create/mechanical_crafting.js
@@ -1,8 +1,42 @@
 'use strict';
 onEvent('recipes', (event) => {
-    const recipes = [];
+    const recipes = [
+        {
+            output: 'astralsorcery:observatory',
+            pattern: [
+                '      ABC',
+                '     DBCB',
+                '    EBCBA',
+                '   FBJBG ',
+                '  ABKBH  ',
+                ' LB BI   ',
+                'M CBAO   ',
+                'MN LPO   ',
+                'MMMOOO   '
+            ],
+            key: {
+                A: 'resourcefulbees:tainted_honeycomb',
+                B: 'astralsorcery:marble_runed',
+                C: 'kubejs:observatory_lens',
+                D: 'quark:lime_rune',
+                E: 'quark:orange_rune',
+                F: 'quark:red_rune',
+                G: 'quark:magenta_rune',
+                H: 'quark:blue_rune',
+                I: 'quark:light_blue_rune',
+                J: 'quark:black_rune',
+                K: '#botania:runes/asgard',
+                L: '#forge:nuggets/brass',
+                M: '#forge:plates/brass',
+                N: '#create:seats',
+                O: 'create:brass_casing',
+                P: 'immersiveengineering:furnace_heater'
+            },
+            id: 'astralsorcery:observatory'
+        }
+    ];
 
     recipes.forEach((recipe) => {
-        event.recipes.create.mechanical_crafting(recipe.result, recipe.pattern, recipe.key);
+        event.recipes.create.mechanical_crafting(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
     });
 });
--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/create/mechanical_crafting.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/create/mechanical_crafting.js
@@ -18,39 +18,6 @@ onEvent('recipes', (event) => {
             id: 'astralsorcery:altar/hand_telescope'
         },
         {
-            output: 'astralsorcery:observatory',
-            pattern: [
-                '      ABC',
-                '     DBCB',
-                '    EBCBA',
-                '   FBJBG ',
-                '  ABKBH  ',
-                ' LB BI   ',
-                'M CBAO   ',
-                'MN LPO   ',
-                'MMMOOO   '
-            ],
-            key: {
-                A: 'resourcefulbees:tainted_honeycomb',
-                B: 'astralsorcery:marble_runed',
-                C: 'kubejs:observatory_lens',
-                D: 'quark:lime_rune',
-                E: 'quark:orange_rune',
-                F: 'quark:red_rune',
-                G: 'quark:magenta_rune',
-                H: 'quark:blue_rune',
-                I: 'quark:light_blue_rune',
-                J: 'quark:black_rune',
-                K: '#botania:runes/asgard',
-                L: '#forge:nuggets/brass',
-                M: '#forge:plates/brass',
-                N: '#create:seats',
-                O: 'create:brass_casing',
-                P: 'immersiveengineering:furnace_heater'
-            },
-            id: 'astralsorcery:observatory'
-        },
-        {
             output: 'appliedenergistics2:controller',
             pattern: ['ACACA', 'CDBDC', 'AFEFA', 'CDBDC', 'ACACA'],
             key: {
