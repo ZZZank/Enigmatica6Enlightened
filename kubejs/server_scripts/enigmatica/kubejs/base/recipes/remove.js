9518020563cfeaa3793dd99eabebfa9c11f31873
ZZZank <3410764033@qq.com>
2024-04-09 21:06:52 +0800

    remove lodestone melting to netherite
    since its recipe is simplified now

--- kubejs/server_scripts/enigmatica/kubejs/base/recipes/remove.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/recipes/remove.js
@@ -285,6 +285,7 @@ onEvent('recipes', (event) => {
         'tconstruct:smeltery/melting/metal/gold/powered_rail', //to prevent dupe loop with Transport
         'tconstruct:smeltery/melting/metal/gold/enchanted_apple',
         'tconstruct:smeltery/melting/metal/gold/apple',
+        'tconstruct:smeltery/melting/metal/netherite/lodestone',
         /tconstruct:smeltery\/melting\/metal\/\w+\/wire/,
         /tconstruct:smeltery\/casting\/filling\/(seared|scorched)_\w+/,
 
