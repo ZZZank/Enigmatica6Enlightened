264c7a2457ac9de83c2bb0cd807da05d859570a4
ZZZank <3410764033@qq.com>
2024-04-09 19:23:20 +0800

    makes crafting botania:laputa_shard harder in normal mode

--- kubejs/server_scripts/enigmatica/kubejs/normal/recipes/replace_input.js
+++ kubejs/server_scripts/enigmatica/kubejs/normal/recipes/replace_input.js
@@ -6,22 +6,27 @@ onEvent('recipes', (event) => {
 
     const recipes = [
         {
-            replaceTarget: { id: 'compactmachines:personal_shrinking_device' },
+            filter: { id: 'botania:laputa_shard' },
+            toReplace: 'botania:life_essence',
+            replaceWith: 'botania:gaia_ingot'
+        },
+        {
+            filter: { id: 'compactmachines:personal_shrinking_device' },
             toReplace: 'minecraft:book',
             replaceWith: 'shrink:shrinking_device'
         },
         {
-            replaceTarget: { id: 'powah:crafting/player_tranmitter_basic' },
+            filter: { id: 'powah:crafting/player_tranmitter_basic' },
             toReplace: 'powah:player_transmitter_starter',
             replaceWith: 'powah:player_aerial_pearl'
         },
         {
-            replaceTarget: { mod: 'powah' },
+            filter: { mod: 'powah' },
             toReplace: '#powah:furnator',
             replaceWith: 'minecraft:blast_furnace'
         },
         {
-            replaceTarget: { mod: 'powah' },
+            filter: { mod: 'powah' },
             toReplace: '#powah:energy_hopper',
             replaceWith: 'minecraft:hopper'
         }
@@ -29,14 +34,14 @@ onEvent('recipes', (event) => {
 
     powahTiers.forEach(function (tier) {
         recipes.push({
-            replaceTarget: { id: `powah:crafting/energizing_rod_${tier}` },
+            filter: { id: `powah:crafting/energizing_rod_${tier}` },
             toReplace: '#powah:energizing_rod',
             replaceWith: `powah:energy_cable_${tier}`
         });
     });
 
     recipes.forEach((recipe) => {
-        event.replaceInput(recipe.replaceTarget, recipe.toReplace, recipe.replaceWith);
+        event.replaceInput(recipe.filter, recipe.toReplace, recipe.replaceWith);
     });
 
     const alt_material_tag_replacements = [
