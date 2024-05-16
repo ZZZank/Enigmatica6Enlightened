1aab7a4d48d47f983bb11a5dab70d9d8688b778e
ZZZank <3410764033@qq.com>
2024-05-16 08:25:39 +0800

    trades flipping implemented via KubeJS
    cool

---
+++ kubejs/server_scripts/enigmatica/kubejs/base/item/trade_flip.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const $VillagerEntity = java('net.minecraft.entity.merchant.villager.VillagerEntity');
+const $MerchantOffers = java('net.minecraft.item.MerchantOffers');
+const $MerchantOffer = java('net.minecraft.item.MerchantOffer');
+
+/**
+ * "Flip" trades of a villager if renamed to/from "Dinnerbone"
+ */
+onEvent('item.entity_interact', (event) => {
+    //filtering: general
+    const { item, hand, target } = event;
+    if (
+        item.id != 'minecraft:name_tag' ||
+        hand != MAIN_HAND ||
+        !(target.minecraftEntity instanceof $VillagerEntity)
+    ) {
+        return;
+    }
+    const mcEntity = target.minecraftEntity;
+
+    //filtering: name
+    const dinnerboneVillager = mcEntity.hasCustomName()
+        ? mcEntity.getCustomName().getString() == 'Dinnerbone'
+        : false;
+    const dinnerboneNameTag = item.getName().string == 'Dinnerbone';
+    if (
+        //target name -> other name
+        !(dinnerboneVillager && !dinnerboneNameTag) &&
+        //other name -> target name
+        !(!dinnerboneVillager && dinnerboneNameTag)
+    ) {
+        return;//skip if no condition matches
+    }
+
+    //flipping trades(sound like swearing)
+    const flipped = new $MerchantOffers();
+    mcEntity.offers.forEach((offer) => {
+        // @ts-ignore
+        flipped.add(
+            //sellingStack <-> buyingStackFirst
+            new $MerchantOffer(
+                offer.sellingStack,
+                offer.buyingStackSecond,
+                offer.buyingStackFirst,
+                0,
+                offer.maxUses,
+                offer.givenExp,
+                offer.priceMultiplier,
+                offer.demand
+            )
+        );
+    });
+    mcEntity.setOffers(flipped);
+});
