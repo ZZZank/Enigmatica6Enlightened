a1b02a28f6c38989273beed690a609b073d1eeb2
ZZZank <3410764033@qq.com>
2024-05-26 12:20:29 +0800

    try to add custom mek registries

--- kubejs/startup_scripts/mek.js
+++ kubejs/startup_scripts/mek.js
@@ -3,6 +3,7 @@
 const $GasDeferredRegister = java('mekanism.common.registration.impl.GasDeferredRegister');
 const $SlurryDeferredRegister = java('mekanism.common.registration.impl.SlurryDeferredRegister');
 const $InfuseTypeDeferredRegister = java('mekanism.common.registration.impl.InfuseTypeDeferredRegister');
+const $MinecraftForge = java("net.minecraftforge.common.MinecraftForge")
 
 const GASES = new $GasDeferredRegister('kubejs');
 const SLURRY = new $SlurryDeferredRegister('kubejs');
@@ -12,8 +13,6 @@ GASES.register('example_gas', 0xa020f0);
 SLURRY.register('example_slurry', (builder) => builder.color(0xa020f0));
 INFUSETYPE.register('example_infuse_type', 0xa020f0);
 
-/*
-GASES.register($EventBuses.getModEventBus('kubejs').get());
-SLURRY['register(net.minecraftforge.eventbus.api.IEventBus)']($EventBuses.getModEventBus('kubejs').get());
-INFUSETYPE.register($EventBuses.getModEventBus('kubejs').get());
-*/
+GASES.register($MinecraftForge.EVENT_BUS);
+SLURRY['register(net.minecraftforge.eventbus.api.IEventBus)']($MinecraftForge.EVENT_BUS);
+INFUSETYPE.register($MinecraftForge.EVENT_BUS);
