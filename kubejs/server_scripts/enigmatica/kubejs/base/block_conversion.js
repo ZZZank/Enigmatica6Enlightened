3c43165d63f26a8401f178637b0773f106d7d378
ZZZank <3410764033@qq.com>
2024-12-07 14:00:29 +0800

    rewrite block conversion to use KubeJEI instead of custom machinery

--- kubejs/server_scripts/enigmatica/kubejs/base/block_conversion.js
+++ kubejs/server_scripts/enigmatica/kubejs/base/block_conversion.js
@@ -1,140 +1,66 @@
 'use strict';
 
 block_conversion: {
+    const $ItemStackJS = java("packages/dev/latvian/kubejs/item/$ItemStackJS")
     const id_prefix = 'enlightened6:right_click_block/';
-    /**
-     * @type {{target:string,output:Internal.ItemStackJS_,holding:string,
-     * additional?:(e:Internal.BlockRightClickEventJS)=>void,id:string}[]}
-     */
-    const recipes = [
-        {
-            target: 'redstone_arsenal:flux_gem_block',
-            output: '4x bloodmagic:largebloodstonebrick',
-            holding: 'bloodmagic:weakbloodshard',
-            id: 'largebloodstonebrick'
-        },
-        {
-            target: 'mekanism:teleporter_frame',
-            output: 'kubejs:conductive_frame',
-            holding: 'immersiveengineering:dust_hop_graphite',
-            id: 'conductive_frame'
-        },
-        {
-            target: 'astralsorcery:marble_raw',
-            output: 'astralsorcery:marble_runed',
-            holding: 'naturesaura:gold_leaf',
-            id: `marble_runed`
-        },
-        {
-            target: 'appliedenergistics2:crafting_unit',
-            output: 'appliedenergistics2:crafting_accelerator',
-            holding: 'appliedenergistics2:engineering_processor',
-            id: `crafting_accelerator`
-        },
-        {
-            target: 'appliedenergistics2:crafting_unit',
-            output: 'appliedenergistics2:1k_crafting_storage',
-            holding: 'appliedenergistics2:1k_cell_component',
-            id: `1k_crafting_storage`
-        },
-        {
-            target: 'appliedenergistics2:crafting_unit',
-            output: 'appliedenergistics2:4k_crafting_storage',
-            holding: 'appliedenergistics2:4k_cell_component',
-            id: `4k_crafting_storage`
-        },
-        {
-            target: 'appliedenergistics2:crafting_unit',
-            output: 'appliedenergistics2:16k_crafting_storage',
-            holding: 'appliedenergistics2:16k_cell_component',
-            id: `16k_crafting_storage`
-        },
-        {
-            target: 'appliedenergistics2:crafting_unit',
-            output: 'appliedenergistics2:64k_crafting_storage',
-            holding: 'appliedenergistics2:64k_cell_component',
-            id: `64k_crafting_storage`
-        },
-        {
-            target: 'minecraft:conduit',
-            output: Item.of('minecraft:trident', {
-                nullptrType: 1,
-                Damage: 225,
-                display: {
-                    Name: '[{ "translate": "item.en6e.null_pointer" }]',
-                    Lore: ['[{ "translate": "lore.en6e.null_pointer" }]']
-                },
-                'quark:RuneAttached': true,
-                'quark:RuneColor': { id: 'quark:gray_rune', Count: 1 }
-            }).enchant('minecraft:vanishing_curse', 1),
-            holding: 'pneumaticcraft:nuke_virus',
-            id: 'null_pointer'
-        },
-        {
-            target: 'minecraft:conduit',
-            output: Item.of('minecraft:trident', {
-                nullptrType: 1,
-                Damage: 225,
-                display: {
-                    Name: '[{ "translate": "item.en6e.null_pointer" }]',
-                    Lore: ['[{ "translate": "lore.en6e.null_pointer" }]']
-                },
-                'quark:RuneAttached': true,
-                'quark:RuneColor': { id: 'quark:gray_rune', Count: 1 }
-            }).enchant('minecraft:vanishing_curse', 1),
-            holding: 'pneumaticcraft:stop_worm',
-            id: 'null_pointer_alt'
-        }
-    ];
+    /** @type {{[x: Special.Block]: {holding:InstanceType<$ItemStackJS>, output: InstanceType<$ItemStackJS>, additional?:(e:$BlockRightClickEventJS_)=>void}[]}} */
+    const compiledRecipes = global['block_conversion']['compiled'];
 
     onEvent('block.right_click', (event) => {
-        const player = event.player;
-        if (!player || player.fake || player.mainHandItem.empty || !player.crouching) {
+        const { player, item, hand } = event
+        if (!player
+            || player.fake
+            || !player.crouching
+            || item.empty
+            || hand != Hand.MAIN_HAND
+        ) {
             return;
         }
 
-        const mainHandItem = player.mainHandItem.id;
         const target = event.block;
+        const recipes = compiledRecipes[target.id]
+        if (!recipes) {
+            return
+        }
+
         for (let recipe of recipes) {
-            if (mainHandItem != recipe.holding || target.id != recipe.target) {
+
+            if (!recipe.holding.test(item)) {
                 continue;
             }
+            console.log('got it')
             player.playSound('ping:bloop');
             event.server.runCommandSilent(`particle minecraft:explosion ${target.x} ${target.y} ${target.z}`);
-            const output = Item.of(recipe.output);
-            if (output.count == 1 && output.isBlock()) {
-                target.set(output.id, output.nbt);
-            } else {
-                target.set(air);
-                const drop = event.world.createEntity('minecraft:item');
-                // @ts-ignore
-                drop.item = output;
-                drop.setPosition(target);
-                drop.setMotionY(0.2);
-                drop.spawn();
-            }
+
+            target.set(air);
             player.mainHandItem.count -= 1;
+
+            let drop = event.world.createEntity('minecraft:item');
+            // @ts-ignore
+            drop.item = recipe.output;
+            drop.setPosition(target);
+            drop.setMotionY(0.2);
+            drop.spawn();
+
+            if (recipe.additional) {
+                recipe.additional(event)
+            }
+
             event.cancel();
             return;
         }
     });
 
-    onEvent('recipes', (event) => {
-        recipes.forEach((recipe) => {
-            // create deploying
-            event.recipes.create
-                .deploying(recipe.output, [recipe.target, recipe.holding])
+    onEvent('recipes', event => {
+        const { deploying } = event.recipes.create
+        /**
+         * @type {{target:Special.Block,output:$ItemStackJS_,holding:$ItemStackJS_,id:string,
+        * additional?:(e:$BlockRightClickEventJS_)=>void}[]}
+        */
+        const recipes = global['block_conversion']['raw_recipes']
+        recipes.forEach(recipe => {
+            deploying(recipe.output, [recipe.target, recipe.holding])
                 .id(id_prefix + recipe.id + '/deploy');
-            // hint
-            const builder = event.recipes.custommachinery
-                .custom_machine('enlightened6:in_world_interaction', 1)
-                .jei();
-            addCMRecipe(builder, {
-                inputs: [recipe.target],
-                catalyst: recipe.holding,
-                outputs: [recipe.output],
-                id: id_prefix + recipe.id + '/hint'
-            });
-        });
-    });
+        })
+    })
 }
---
+++ kubejs/startup_scripts/data_compile/block_conversion.js
@@ -0,0 +1,119 @@
+// priority: 10000
+
+
+{
+    const $IngredientJS = java("packages/dev/latvian/kubejs/item/ingredient/$IngredientJS")
+    const $ItemStackJS = java("packages/dev/latvian/kubejs/item/$ItemStackJS")
+
+    global['block_conversion'] = {
+        /**
+         * @type {{target:Special.Block,output:$ItemStackJS_,holding:$ItemStackJS_,id:string,
+         * additional?:(e:$BlockRightClickEventJS_)=>void}[]}
+         */
+        raw_recipes: [
+            {
+                target: 'redstone_arsenal:flux_gem_block',
+                output: '4x bloodmagic:largebloodstonebrick',
+                holding: 'bloodmagic:weakbloodshard',
+                id: 'largebloodstonebrick'
+            },
+            {
+                target: 'mekanism:teleporter_frame',
+                output: 'kubejs:conductive_frame',
+                holding: 'immersiveengineering:dust_hop_graphite',
+                id: 'conductive_frame'
+            },
+            {
+                target: 'astralsorcery:marble_raw',
+                output: 'astralsorcery:marble_runed',
+                holding: 'naturesaura:gold_leaf',
+                id: `marble_runed`
+            },
+            {
+                target: 'appliedenergistics2:crafting_unit',
+                output: 'appliedenergistics2:crafting_accelerator',
+                holding: 'appliedenergistics2:engineering_processor',
+                id: `crafting_accelerator`
+            },
+            {
+                target: 'appliedenergistics2:crafting_unit',
+                output: 'appliedenergistics2:1k_crafting_storage',
+                holding: 'appliedenergistics2:1k_cell_component',
+                id: `1k_crafting_storage`
+            },
+            {
+                target: 'appliedenergistics2:crafting_unit',
+                output: 'appliedenergistics2:4k_crafting_storage',
+                holding: 'appliedenergistics2:4k_cell_component',
+                id: `4k_crafting_storage`
+            },
+            {
+                target: 'appliedenergistics2:crafting_unit',
+                output: 'appliedenergistics2:16k_crafting_storage',
+                holding: 'appliedenergistics2:16k_cell_component',
+                id: `16k_crafting_storage`
+            },
+            {
+                target: 'appliedenergistics2:crafting_unit',
+                output: 'appliedenergistics2:64k_crafting_storage',
+                holding: 'appliedenergistics2:64k_cell_component',
+                id: `64k_crafting_storage`
+            },
+            {
+                target: 'minecraft:conduit',
+                output: Item.of('minecraft:trident', {
+                    nullptrType: 1,
+                    Damage: 225,
+                    display: {
+                        Name: '[{ "translate": "item.en6e.null_pointer" }]',
+                        Lore: ['[{ "translate": "lore.en6e.null_pointer" }]']
+                    },
+                    'quark:RuneAttached': true,
+                    'quark:RuneColor': { id: 'quark:gray_rune', Count: 1 }
+                }).enchant('minecraft:vanishing_curse', 1),
+                holding: 'pneumaticcraft:nuke_virus',
+                id: 'null_pointer'
+            },
+            {
+                target: 'minecraft:conduit',
+                output: Item.of('minecraft:trident', {
+                    nullptrType: 1,
+                    Damage: 225,
+                    display: {
+                        Name: '[{ "translate": "item.en6e.null_pointer" }]',
+                        Lore: ['[{ "translate": "lore.en6e.null_pointer" }]']
+                    },
+                    'quark:RuneAttached': true,
+                    'quark:RuneColor': { id: 'quark:gray_rune', Count: 1 }
+                }).enchant('minecraft:vanishing_curse', 1),
+                holding: 'pneumaticcraft:stop_worm',
+                id: 'null_pointer_alt'
+            }
+        ],
+        /** @type {{[x: Special.Block]: {holding:InstanceType<$ItemStackJS>, output: InstanceType<$ItemStackJS>, additional?:(e:$BlockRightClickEventJS_)=>void}[]}} */
+        compiled: {}
+    }
+
+    /**
+     * @type {{
+     * [x: Special.Block]: {
+     *  holding:InstanceType<$IngredientJS>,
+     *  output: InstanceType<$ItemStackJS>,
+     *  additional?:(e:$BlockRightClickEventJS_)=>void
+     * }[]}}
+     */
+    const compiled = {}
+    global['block_conversion']['raw_recipes'].forEach(recipe => {
+        const {target, holding, output, additional} = recipe
+        if (!compiled[target]) {
+            compiled[target] = []
+        }
+        const targeted = compiled[target]
+        targeted.push({
+            holding: Ingredient.of(holding),
+            output: Item.of(output),
+            additional: additional
+        })
+    })
+    global['block_conversion']['compiled'] = compiled
+}
